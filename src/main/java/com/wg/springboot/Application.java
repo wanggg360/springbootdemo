83eba568036272e4c828d33038de2381f8ee4514
wanggg360 <wanggang_sd@163.com>
2018-12-01 22:36:29 +0800

    新增视图解析

--- src/main/java/com/wg/springboot/Application.java
+++ src/main/java/com/wg/springboot/Application.java
@@ -1,16 +1,24 @@
 package com.wg.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-
+@EnableAutoConfiguration
 @SpringBootApplication
 
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(Application.class, args);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
 
     }
-}
+        public static void main (String[] args) throws Exception {
+
+            SpringApplication.run(Application.class, args);
+
+        }
+    }
+
--- src/main/java/com/wg/springboot/controller/LoginController.java
+++ src/main/java/com/wg/springboot/controller/LoginController.java
@@ -2,12 +2,13 @@ package com.wg.springboot.controller;
 
 import com.wg.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 public class LoginController {
 
     private UserService userService;
@@ -17,8 +18,6 @@ public class LoginController {
     public String loginPage() {
         return "login";
     }
-
-
 
     @RequestMapping(value = "/loginCheck.html")
 
@@ -36,7 +35,6 @@ public class LoginController {
 
 
     }
-
 
     @Autowired
     public void setUserService(UserService userService) {
